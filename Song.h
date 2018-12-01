c3274f47abf77ba7d12450262dc7a90d559fcd4c
mcquill99 <mcquill99@gmail.com>
2018-12-01 18:32:07 -0500

    Finished .h for playListCollection and constructor for .cpp

--- Song.h
+++ Song.h
@@ -15,11 +15,14 @@ private:
     int playCount;
 public:
     Song(std::string artistIn, std::string titleIn, double durationIn, int playCountIn=0);
+    Song(const Song& songToCopy);
+    Song& operator=(const Song& songToCopy);
     std::string getArtist();
     std::string getTitle();
     double getDuration();
     int getPlayCount();
     void updatePlayCount();
+    std::string toString();
 
 };
 
