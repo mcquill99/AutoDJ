6d7e892fa6add667fa69d4f55b6adce502281ff1
eostendarp <eostendarp@ithaca.edu>
2018-12-03 18:02:46 -0500

    FileIO

---
+++ FileIO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eostendarp on 12/3/18.
+//
+
+#include "FileIO.h"
---
+++ FileIO.h
@@ -0,0 +1,14 @@
+//
+// Created by eostendarp on 12/3/18.
+//
+
+#ifndef AUTODJ_FILEIO_H
+#define AUTODJ_FILEIO_H
+
+
+class FileIO {
+
+};
+
+
+#endif //AUTODJ_FILEIO_H
