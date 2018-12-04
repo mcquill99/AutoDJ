b97108c3fd90cea85f8f59f7d0bc9b34383485e5
mcquill99 <mcquill99@gmail.com>
2018-12-04 17:18:37 -0500

    made useless changes

--- testMain.cpp
+++ testMain.cpp
@@ -2,9 +2,13 @@
 // Created by mcqui on 12/3/2018.
 //
 #include <iostream>
+#include "Song.h"
+#include "PlayList.h"
+#include "PlayListsCollection.h"
 
 int main(){
     std::cout << "-----Song-----" << std::endl;
+    Song newSong = Song("Darude", "SandStorm", 4.59);
 
 
 
