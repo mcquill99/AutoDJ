9916d4a2657ed6080c7fa70608e034412f2f8e7d
mcquill99 <mcquill99@gmail.com>
2018-12-04 09:33:22 -0500

    added testLib and layed out test cpp

--- testMain.cpp
+++ testMain.cpp
@@ -1,7 +1,21 @@
 //
 // Created by mcqui on 12/3/2018.
 //
+#include <iostream>
 
-main(){
+int main(){
+    std::cout << "-----Song-----" << std::endl;
 
+
+
+
+    std::cout << "-----PlayList-----" << std::endl;
+
+
+
+
+    std::cout << "-----PlayListsCollection-----" << std::endl;
+
+
+    return 0;
 }
